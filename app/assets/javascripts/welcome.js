71ba18305bf48e7301e85d80c5346d6147e2ae1c
nathanallen <thenathanator@gmail.com>
2016-05-09 15:52:08 -0700

    cleanup

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,14 +3,15 @@
 
 
 var page_view_count = {
-  "/": 0,
-  "/goldenrod": 0,
-  "/lightcoral": 0,
-  "/lightseagreen": 0
+  "home": 0,
+  "goldenrod": 0,
+  "lightcoral": 0,
+  "lightseagreen": 0
 }
 
-$(document).on("ready", function(){
-  var current_path = location.pathname;
+
+$(document).on("page:change", function(){
+  var current_path = location.pathname.split("/")[1] || "home";
   page_view_count[current_path] += 1;
-  $("#home .view_count.badge").text(page_view_count[current_path]);
+  $("#" + current_path + " .view_count.badge").text(page_view_count[current_path]);
 })

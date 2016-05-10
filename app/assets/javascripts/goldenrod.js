1a72e4008d2af8f736e61dd67befef9669edf65e
nathanallen <thenathanator@gmail.com>
2016-05-10 13:34:20 -0700

    solution: localStorage bonus

--- app/assets/javascripts/goldenrod.js
+++ app/assets/javascripts/goldenrod.js
@@ -5,6 +5,11 @@ $(document).on("ready", function(){
 
   var $goldenrod = $("body.goldenrod.index");
   if ( !$goldenrod.length ){ return; } // ensures we're on the right page!
-  alert("Welcome to teh best page!")
+
+  var has_been_here_before = localStorage.getItem("was_welcomed_to_teh_best_page");
+  if ( !has_been_here_before ){
+    alert("Welcome to teh best page!")
+    localStorage.setItem("was_welcomed_to_teh_best_page", "1")
+  }
 
 })

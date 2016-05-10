41e2bf1f49f42520dae63c8d5115134b21e722d2
nathanallen <thenathanator@gmail.com>
2016-05-10 13:30:01 -0700

    solution: conditional js

--- app/assets/javascripts/goldenrod.js
+++ app/assets/javascripts/goldenrod.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).on("ready", function(){
+
+  var $goldenrod = $("body.goldenrod.index");
+  if ( !$goldenrod.length ){ return; } // ensures we're on the right page!
+  alert("Welcome to teh best page!")
+
+})
